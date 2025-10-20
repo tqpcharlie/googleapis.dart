13210b2c78edf23941cae6c8f1c6300f21768ae3
Charlotte <69423184+tqpcharlie@users.noreply.github.com>
2025-10-20 01:12:56 +0000

    bump

--- drive.dart
+++ drive.dart
@@ -1,2 +1,38 @@
-import '../generated/googleapis/lib/drive/v2.dart';
+import 'package:googleapis.dart/generated/googleapis/lib/drive/v2.dart';
+import 'package:drive_helper/drive_helper.dart';
 
+
+late DriveHelper driveHelper;
+UploadOptions test = uploadOptions();
+
+DriveHelper.initialise(
+    driveScopes: [DriveApi.DriveScope]
+);
+
+  @override
+  Widget build() {
+    return FutureBuilder() {
+      future: () async => driveHelper = await driveHelper.initialise(
+        driveScopes: [DriveApi.DriveScope],
+      ),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
+          // Return your app's homepage
+        } else if (snapshot.hasError) {
+          // Return an error page
+        } else {
+          // Return a progress indicator like `CircularProgressIndicator`
+      }
+    }
+  }
+
+DriveHelper.createFile(
+  'test.txt',
+  'text/plain',
+  'Hello, world!',
+  uploadOptions: test,
+).then((file) {
+  print('File uploaded: ${file.id}');
+}).catchError((error) {
+  print('Error uploading file: $error');
+});
